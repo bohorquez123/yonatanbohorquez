3d47872d495a2d63153a4585b65afa6fe527808b
yonatan bohorquez <xdolmario@gmail.com>
2026-05-04 17:09:26 -0500

    Renderizar lista de items con .map() y datos quemados

--- nba_yb/src/App.jsx
+++ nba_yb/src/App.jsx
@@ -6,6 +6,16 @@ import Item from './componentes/Item'
 import PiePagina from './componentes/PiePagina'
 import './App.css'
 function App() {
+  const peliculas = [
+    {id: 1, titulo: "Encanto", estrellas: 5, visto: true },
+    {id: 2, titulo: "Coco", estrellas : 4, visto: true },
+    {id: 3, titulo: "Soul", estrellas: 5, visto: false }
+  ]
+  const tareas = [
+    {id: 1, texto: "Esudiar React", completada: false },
+    {id: 2, texto: "Hacer Ejercicio", completada: true },
+    {id: 3, texto: "leer 10 paginas", completada: false }
+  ]
   const tareaEjemplo = {
         id : 1,
         texto : "Estudiar React",
@@ -17,7 +27,8 @@ function App() {
       titulo= "mis tareas"
       subtitulo= "organiza lo que tienes qu hacer hoy" />
       <Formulario />
-      <Lista />
+      <Lista tareas={peliculas} />
+      
       
       <Item tarea={tareaEjemplo}/>
       <PiePagina />
